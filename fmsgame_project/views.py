ce8342f8d9d89950bf48121896c3087ae163a0bd
Dave Whiteland <dave@mysociety.org>
2010-12-22 16:15:44 +0000

    initial issue page routing

--- fmsgame_project/views.py
+++ fmsgame_project/views.py
@@ -1,10 +1,20 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from django.http import Http404, HttpResponseRedirect
 
 @login_required
 def issue(request, issue_id=None):
     if request.method == 'post':
-        pass
+        state = request.POST.get('state')
+        if state == 'fixed':
+            pass
+        elif state == 'notfixed':
+            pass
+        elif state == 'notfound':
+            pass
+        else:
+            raise Http404
+        return HttpResponseRedirect(reverse('geolocate'))
     return render_to_response('issue.html')
 
     
