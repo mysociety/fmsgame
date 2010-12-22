d4f88f71aa1bb270b212b5ea8604cbe8d6e2e75a
Dave Whiteland <dave@mysociety.org>
2010-12-22 15:34:26 +0000

    added login required

--- fmsgame_project/views.py
+++ fmsgame_project/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 
+@login_required
 def issue(request, issue_id=None):
     if request.method == 'post':
         pass
