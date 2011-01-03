50cd8dbc7b6e541c0dab7e0630a70676a9d93966
Duncan Parkes <duncan@mysociety.org>
2011-01-03 20:55:03 +0000

    Send confirmation emails to a shiny new gmail account.

--- fmsgame_project/views.py
+++ fmsgame_project/views.py
@@ -38,7 +38,7 @@ def issue(request, issue_id=None):
         data =  {'submit_update': '1',
                  'id': issue_id,
                  'name': request.user.get_full_name(),
-                 'rznvy': 'mysociety.director@gmail.com',  # request.user.email,
+                 'rznvy': 'fmsgame@gmail.com',  # request.user.email,
                  #'email': '', # check this
                  #'update': '', # text of the update (e.g., "I put it in the bin")
                  'fixed': '', # checkbox for: Is it fixed?
