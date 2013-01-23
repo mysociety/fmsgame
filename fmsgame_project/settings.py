958bb7ba7a6350327bde5a050a82b45435bf5500
Edmund von der Burg <evdb@mysociety.org>
2013-01-23 18:13:09 -0300

    Remove the auth from context processors

--- fmsgame_project/settings.py
+++ fmsgame_project/settings.py
@@ -118,7 +118,6 @@ INSTALLED_APPS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
-    'django.core.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
